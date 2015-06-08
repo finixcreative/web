73936a381277131a332166529a04fb02a2cd26b5
finix-rsmith <rsmith@finixcreative.com>
2015-06-08 14:10:28 -0400

    Slides - directive
    Comment out linked function

--- app/modules/slides/slides-drv.js
+++ app/modules/slides/slides-drv.js
@@ -7,7 +7,7 @@ angular.module('slidesMod')
 			templateUrl: '/web/app/modules/slides/slides.html',
 			controller: 'SlidesController',
 			controllerAs: 'slides',
-			link: function (scope, elem, attrs){
+			/* link: function (scope, elem, attrs){
 				scope.currentIndex = 0;
 				scope.next = function(){
 					scope.currentIndex < scope.images.length - 1 ? scope.currentIndex++ : scope.currentIndex = 0;
@@ -32,6 +32,6 @@ angular.module('slidesMod')
 				scope.$on('$destroy', function() {
 					$timeout.cancel(timer); // when the scope is getting destroyed, cancel the timer
 				});
-			},
+			}, */
 		};
 	});
