60febc3bb206b738ab746dc7d78b6100af60c60a
finix-rsmith <rsmith@finixcreative.com>
2015-06-15 19:58:14 -0400

    Subsection - directive
    Modify template to accept attribute value

--- app/modules/subsection/subsection-drv.js
+++ app/modules/subsection/subsection-drv.js
@@ -1,22 +1,10 @@
 angular.module('coreMod')
 	.directive('subsection', function(){
 		return {
-			restrict: 'E',
-			replace: true,
-			templateUrl: '/web/app/modules/subsection/subsection.html',
-		};
-	})
-	.directive('subsectionMajor', function(){
-		return {
 			restrict: 'EAC',
 			replace: false,
-			templateUrl: '/web/app/modules/subsection/subsection-major.html',
-		};
-	})
-	.directive('subsectionMinor', function(){
-		return {
-			restrict: 'EAC',
-			replace: false,
-			templateUrl: '/web/app/modules/subsection/subsection-minor.html',
+			templateUrl: function(elem, attr){
+				return '/web/app/modules/subsection/'+attr.template+'.html';
+			}
 		};
 	});
