01fe33febdf8b5bbb27987012a14fa8700868782
finix-rsmith <rsmith@finixcreative.com>
2015-06-15 01:08:10 -0400

    Subsections - templates
    Create two basic templates for subsections

--- app/modules/subsection/subsection-drv.js
+++ app/modules/subsection/subsection-drv.js
@@ -1,8 +1,16 @@
 angular.module('coreMod')
-	.directive('subsection', function(){
+	.directive('subsection-major', function(){
 		return {
 			restrict: 'EAC',
-			templateUrl: '/web/app/modules/subsection/subsection.html',
+			templateUrl: '/web/app/modules/subsection/subsection-major.html',
+			controller: 'SitemapController',
+			controllerAs: 'sitemap',
+		};
+	})
+	.directive('subsection-minor', function(){
+		return {
+			restrict: 'EAC',
+			templateUrl: '/web/app/modules/subsection/subsection-minor.html',
 			controller: 'SitemapController',
 			controllerAs: 'sitemap',
 		};
