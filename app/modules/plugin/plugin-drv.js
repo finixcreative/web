a8b29be36439f01260514a247acc1a2b863d887b
finix-rsmith <rsmith@finixcreative.com>
2015-06-16 14:53:52 -0400

    Plugin - directive
    Create plugin directive to use ng-repeat to generate dynamic markup

---
+++ app/modules/plugin/plugin-drv.js
@@ -0,0 +1,15 @@
+angular.module('coreMod')
+	.directive('plugin', ['$compile', function($compile) {
+		return {
+			restrict: 'E',
+			replace: true,
+			scope: {
+				'plugin': '='
+			},
+			link: function(scope, element) {
+				var template = '<' + scope.plugin + ' />' + '</' + scope.plugin + '>',
+				compiled = $compile(template)(scope);
+				element.append(compiled);            
+			},
+		};
+	}]);
