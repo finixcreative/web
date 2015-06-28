62f9b12c13b53819f6ae06fff53a33f9ac15391c
finix-rsmith <rsmith@finixcreative.com>
2015-06-28 13:27:29 -0400

    Routes - dynamic directive
    Remove "home" route

--- app/routes.js
+++ app/routes.js
@@ -1,11 +1,6 @@
 angular.module('finixApp')
-	.config(['$routeProvider', function($routeProvider) {
+	.config(['$routeProvider', function ($routeProvider) {
 		$routeProvider
-			.when('/home', {
-				templateUrl: 'app/pages/content/content.html',
-				controller: 'ContentController',
-				controllerAs: 'content',
-			})
 			.when('/:pageId', {
 				templateUrl: 'app/pages/content/content.html',
 				controller: 'ContentController',
