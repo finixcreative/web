9b1b245ec23bb7c4314a954d2857d4ae4812b968
finix-rsmith <rsmith@finixcreative.com>
2015-06-28 14:30:15 -0400

    Slides - assets
    Add slides.js to assets/js/

---
+++ assets/js/slides.js
@@ -0,0 +1,20 @@
+var images = [
+	{
+		"background": {
+			"img": "",
+			"color": "",
+			"position": "",
+		},
+		"id": "",
+		"state": "",
+		"headline": "",
+		"subheadline": "",
+		"text": "",
+		"links": [
+			{
+				"text": "",
+				"url": "",
+			},
+		],
+	},
+];
