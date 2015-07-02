b9ff7243b4e97067a7c86032ca1edd85ddcf3b9a
finix-rsmith <rsmith@finixcreative.com>
2015-07-02 18:31:28 -0400

    Elements - directive & dependencies
    Add element directive and dependencies

--- app/pages/content/content-ctrl.js
+++ app/pages/content/content-ctrl.js
@@ -1,4 +1,4 @@
 angular.module('coreMod')
 	.controller('ContentController', ['$scope', '$routeParams', 'Content', function ($scope, $routeParams, Content){
-		$scope.content = Content.get({pageId: $routeParams.pageId});
+		$scope.elements = Content.get({pageId: $routeParams.pageId});
 	});
