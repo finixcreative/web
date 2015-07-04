2cbb171ede9f9061ae8c471d99d48059fe5a90e8
finix-rsmith <rsmith@finixcreative.com>
2015-07-04 10:49:17 -0400

    Content - controller
    Close array in controller

--- app/pages/content/content-ctrl.js
+++ app/pages/content/content-ctrl.js
@@ -1,4 +1,4 @@
 angular.module('coreMod')
 	.controller('ContentController', ['$scope', '$routeParams', 'Content', function ($scope, $routeParams, Content){
 		$scope.elements = Content.get({pageId: $routeParams.pageId});
-	});
+	}]);
