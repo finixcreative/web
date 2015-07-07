64f70313f7b60ab1df5ede34a1729b29334f1a9d
finix-rsmith <rsmith@finixcreative.com>
2015-07-07 10:38:09 -0400

    Element - template & controller
    Update template and controller to model additional data

--- app/modules/element/element-ctrl.js
+++ app/modules/element/element-ctrl.js
@@ -1,5 +1,11 @@
 angular.module('coreMod')
 	.controller('ElementController', function ($scope){
-		$scope.plugin = 'copyright';
+		$scope = {
+			plugin: 'copyright',
+			elements: [
+				{plugin: 'contact'},
+				{plugin: 'social'},
+			],
+		};
 		$scope.createElement = angular.element('<' + $scope.plugin + '></' + $scope.plugin + '>');
 	});
