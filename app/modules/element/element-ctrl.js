b85a26ffd2f144c0cfd3eea441837c6df82931f4
finix-rsmith <rsmith@finixcreative.com>
2015-07-07 10:04:32 -0400

    Revert "Element - controller & directive"
    This reverts commit a3bd103bcadbbe7bfb01b98924a0dfb8b397019a.

--- app/modules/element/element-ctrl.js
+++ app/modules/element/element-ctrl.js
@@ -1,5 +1,4 @@
 angular.module('coreMod')
 	.controller('ElementController', function ($scope){
-		$scope.plugin = '=';
 		$scope.createElement = angular.element('<' + $scope.plugin + '></' + $scope.plugin + '>');
 	});
