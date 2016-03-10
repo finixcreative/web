8b90ebb34ce89626d7b2df43fda6d3fd74da2b45
rsmith-finix <rsmith@finixcreative.com>
2016-03-09 18:32:02 -0800

    Resource - config
    Switch controller to config and add type form to url whitelist

---
+++ app/modules/resource/resource-config.js
@@ -0,0 +1,13 @@
+angular.module('coreMod')
+	.config(function($sceDelegateProvider){
+		$sceDelegateProvider.resourceUrlWhitelist(
+			[
+				// Allow same origin resource loads.
+				'self',
+				// Allow loading from outer templates domain.
+				'http://finixcreative.github.io/web/**',
+				'https://finixcreative.typeform.com/**'
+			]
+		);
+	})
+;
--- app/modules/resource/resource-ctrl.js
+++ app/modules/resource/resource-ctrl.js
@@ -1,5 +1,5 @@
 angular.module('coreMod')
-	.controller('ResourceController', ['$http', '$scope', '$sce', function($http, $scope, $sce){
+	.controller('ResourceController', ['$scope', '$sce', function($scope, $sce){
 		for (var i = 0; i < $scope.contents.length; i++) {
 			var config = $scope.contents[i].config;
 			config.trustedsrc = $sce.trustAsResourceUrl(config.src);
