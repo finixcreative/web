ade7fb1836d9b96f0dcb3261f107f69e5b2b4590
rsmith-finix <rsmith@finixcreative.com>
2016-03-11 23:39:40 -0800

    Graph - build d3 data chart
    Add d3 chart capabilities and build stacked area chart in homepage

---
+++ app/modules/graph/stackedarea.js
@@ -0,0 +1,17 @@
+d3.json('sampledata.json', function(data){
+	nv.addGraph(function(){
+		var chart;
+		chart = nv.models.stackedAreaChart().x(function(d){
+			return d[0]
+		}).y(function(d){
+			return d[1]
+		}).clipEdge(true).useInteractiveGuideline(true);
+		chart.xAxis.showMaxMin(false).tickFormat(function(d){
+			return d3.time.format('%x')(new Date(d))
+		});
+		chart.yAxis.tickFormat(d3.format(',.2f'));
+		d3.select('#chart svg').datum(data).transition().duration(500).call(chart);
+		nv.utils.windowResize(chart.update);
+		return chart;
+	});
+})
