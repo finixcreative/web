2bab1b3176023b701b5bb06889d64200def231f7
rsmith-finix <rsmith@finixcreative.com>
2016-03-15 22:24:11 -0700

    Config - stealth
    Change “hide” and “hidden” to “stealth”

--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -3,14 +3,14 @@ angular.module('coreMod')
 		$scope.configure = false;
 		if($scope.configure === true){
 			$(".hydra").addClass("relative");
-			$(".configpanel").removeClass("hide");
+			$(".configpanel").removeClass("stealth");
 			$scope.configLabel = "Done";
 		} else if($scope.configure === false){
 			$(".hydra").removeClass("relative");
-			$(".configpanel").addClass("hide");
+			$(".configpanel").addClass("stealth");
 			$scope.configLabel = "Edit";
 		} else {
-			$(".configpanel").addClass("hide");
+			$(".configpanel").addClass("stealth");
 			$scope.configLabel = "Error";			
 		}
 		$scope.configPanel = function(){
@@ -18,15 +18,15 @@ angular.module('coreMod')
 			$scope.configure = !$scope.configure;
 			if($scope.configure === true){
 				$(".hydra").addClass("relative");
-				$(".configpanel").removeClass("hide");
+				$(".configpanel").removeClass("stealth");
 				$scope.configLabel = "Done";
 			} else if($scope.configure === false){
 				$(".hydra").removeClass("relative");
-				$(".configpanel").addClass("hide");
+				$(".configpanel").addClass("stealth");
 				$scope.configLabel = "Edit";
 			} else {
-				$(".configpanel").addClass("hide");
-				$scope.configLabel = "Error";			
+				$(".configpanel").addClass("stealth");
+				$scope.configLabel = "Error";
 			}
 			console.log('Configure Update = ' + $scope.configure);
 		};
