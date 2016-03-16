f42c772e3065d8720d433e4efb66db17b5d042d8
rsmith-finix <rsmith@finixcreative.com>
2016-03-15 22:16:04 -0700

    Config - relocate functions
    Move functions into config controller from chimera to avoid scope issues

--- app/modules/chimera/chimera-ctrl.js
+++ app/modules/chimera/chimera-ctrl.js
@@ -10,36 +10,6 @@ angular.module('chimeraMod')
 		$scope.contents = [];
 		$scope.errors = [];
 		$scope.edit = false;
-		$scope.configure = false;
-		if($scope.configure === true){
-			$(".hydra").addClass("relative");
-			$(".configpanel").removeClass("hide");
-			$scope.configLabel = "Done";
-		} else if($scope.configure === false){
-			$(".hydra").removeClass("relative");
-			$(".configpanel").addClass("hide");
-			$scope.configLabel = "Edit";
-		} else {
-			$(".configpanel").addClass("hide");
-			$scope.configLabel = "Error";			
-		}
-		$scope.configPanel = function(){
-			console.log('Configure Init = ' + $scope.configure);
-			$scope.configure = !$scope.configure;
-			if($scope.configure === true){
-				$(".hydra").addClass("relative");
-				$(".configpanel").removeClass("hide");
-				$scope.configLabel = "Done";
-			} else if($scope.configure === false){
-				$(".hydra").removeClass("relative");
-				$(".configpanel").addClass("hide");
-				$scope.configLabel = "Edit";
-			} else {
-				$(".configpanel").addClass("hide");
-				$scope.configLabel = "Error";			
-			}
-			console.log('Configure Update = ' + $scope.configure);
-		};
 		$http.get(pageData).then(
 			function success(response){
 				//success callbacks
--- app/modules/config/config-ctrl.js
+++ app/modules/config/config-ctrl.js
@@ -1,6 +1,34 @@
 angular.module('coreMod')
 	.controller('ConfigController', function($scope){
-		var configure = false;
-		$scope.edit = false;
+		$scope.configure = false;
+		if($scope.configure === true){
+			$(".hydra").addClass("relative");
+			$(".configpanel").removeClass("hide");
+			$scope.configLabel = "Done";
+		} else if($scope.configure === false){
+			$(".hydra").removeClass("relative");
+			$(".configpanel").addClass("hide");
+			$scope.configLabel = "Edit";
+		} else {
+			$(".configpanel").addClass("hide");
+			$scope.configLabel = "Error";			
+		}
+		$scope.configPanel = function(){
+			console.log('Configure Init = ' + $scope.configure);
+			$scope.configure = !$scope.configure;
+			if($scope.configure === true){
+				$(".hydra").addClass("relative");
+				$(".configpanel").removeClass("hide");
+				$scope.configLabel = "Done";
+			} else if($scope.configure === false){
+				$(".hydra").removeClass("relative");
+				$(".configpanel").addClass("hide");
+				$scope.configLabel = "Edit";
+			} else {
+				$(".configpanel").addClass("hide");
+				$scope.configLabel = "Error";			
+			}
+			console.log('Configure Update = ' + $scope.configure);
+		};
 	})
 ;
