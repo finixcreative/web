91495f81dd383f0664ffade3d6d5439b5ca4921b
rsmith-finix <rsmith@finixcreative.com>
2016-03-17 12:59:58 -0700

    Graph - nvd3 syntax
    Update graph syntax for legibility

--- app/modules/graph/stackedarea-nvd3.js
+++ app/modules/graph/stackedarea-nvd3.js
@@ -1,15 +1,20 @@
 d3.json('app/modules/graph/sampledata.json', function(data){
 	nv.addGraph(function(){
-		var chart = nv.models.stackedAreaChart().x(function(d){
-			return d[0]
-		}).y(function(d){
-			return d[1]
-		}).clipEdge(true).useInteractiveGuideline(true);
-		chart.xAxis.showMaxMin(false).tickFormat(function(d){
-			return d3.time.format('%x')(new Date(d))
-		});
+		var chart = nv.models.stackedAreaChart()
+			.x(function(d){return d[0]})
+			.y(function(d){return d[1]})
+			.clipEdge(true)
+			.useInteractiveGuideline(true);
+		chart.xAxis
+			.showMaxMin(false)
+			.tickFormat(function(d){return d3.time
+				.format('%x')(new Date(d))
+			});
 		chart.yAxis.tickFormat(d3.format(',.2f'));
-		d3.select('#chart svg').datum(data).transition().duration(500).call(chart);
+		d3.select('#chart svg')
+			.datum(data)
+			.transition().duration(500)
+			.call(chart);
 		nv.utils.windowResize(chart.update);
 		return chart;
 	});
