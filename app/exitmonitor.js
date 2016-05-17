962a18fb245f22f9a9d60a68239bbdfe60a9aa2c
rsmith-finix <rsmith@finixcreative.com>
2016-05-17 14:01:48 -0700

    Exitmonitor - conditional case
    Use correct comparator in conditional

--- app/exitmonitor.js
+++ app/exitmonitor.js
@@ -1,17 +1,8 @@
 window.onload = function(){
-	var exitCount, exitMonitor;
-	/*
-	var saved;
-	$('a').on('click', function(){
-		saved = true;
-	});
-	$('form').on('submit', function(){
-		saved = true;
-	});
-	*/
-	exitCount = 0;
+	var exitCount = 0,
+		exitMonitor;
 	exitMonitor = function(){
-		if(exitCount = 0 && confirm("Are you suuuuure?")){
+		if(exitCount === 0 && confirm("Are you suuuuure?")){
 			exitCount++;
 			return;
 		} else {
