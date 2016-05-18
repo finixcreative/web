f28c2532dc3a1afe23c4c82d9dba4db5aba3ee79
rsmith-finix <rsmith@finixcreative.com>
2016-05-17 23:01:14 -0700

    Exitmonitor - dismiss button
    Use jQuery to apply DOM changes

--- app/exitmonitor.js
+++ app/exitmonitor.js
@@ -1,9 +1,10 @@
 window.onload = function(){
 	var exitCount,
-		exitMonitor,
 		template,
+		exitMonitor,
 		dismiss;
 	exitCount = 0;
+	template = "<div id='exitmonitor' class='pad-40-20 full'><h3>Wait up!</h3><button id='dismiss'>Nah...</button></div>";
 	exitMonitor = function(){
 		if(exitCount === 0){
 			$('#content').prepend(template);
@@ -21,9 +22,8 @@ window.onload = function(){
 			return;
 		}
 	};
-	template = "<div id='exitmonitor' class='pad-40-20 full'><h3>Wait up!</h3><button onclick='dismiss()'>nah</button></div>";
-	dismiss = function(){
-		$('#exitmonitor').remove();		
-	};
+	$('button #dismiss').click(function(){
+		$('#exitmonitor').remove();
+	});
 	window.onmouseout = exitMonitor;
 };
