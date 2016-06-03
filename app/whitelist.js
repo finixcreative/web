3141b8860108ac3fca2eb0b6a14225c547dde396
rsmith-finix <rsmith@finixcreative.com>
2016-06-02 23:09:06 -0700

    Whitelist - set up whitelist script
    Add whitelist file and script tag

---
+++ app/whitelist.js
@@ -0,0 +1,12 @@
+angular.module('finixApp')
+	.config(['$sceDelegateProvider', function($sceDelegateProvider){
+		$sceDelegateProvider.resourceUrlWhitelist([
+			// Allow same origin resource loads.
+			'self',
+			// Allow loading from our assets domain.  Notice the difference between * and **.
+			'https://www.google.com/**',
+			'https://www.youtube.com/**',
+			'https://twitter.com/**'
+		]);
+	}])
+;
