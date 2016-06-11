aeeaf7c61e261bd9ceef7acf0165ff05aba1121f
rsmith-finix <rsmith@finixcreative.com>
2016-06-11 11:50:02 -0700

    Exitmonitor - script
    Update script to load external content resources

--- app/modules/exitmonitor/exitmonitor.js
+++ app/modules/exitmonitor/exitmonitor.js
@@ -1,9 +1,23 @@
-window.onload = function(){
-	var exitCount,
-		template,
+window.onload = function($scope, $http){
+	var exitCount = 0,
+		pageData = 'app/contents/exitmonitor.json',
 		exitMonitor;
-	exitCount = 0;
-	template = "<exitmonitor></exitmonitor>";
+	$scope.template = 'app/modules/hydra/hydra.html';
+	$scope.contents = [];
+	$http.get(pageData).then(
+		function success(response){
+			//success callbacks
+			$scope.contents = response.data;
+		},
+		function error(response){
+			//error callbacks
+			$scope.contents = response.data;
+			$scope.errors = response.error;
+			if($scope.errors == undefined){
+				$location.url("/404");
+			};
+		}
+	);
 	exitMonitor = function(){
 		if(exitCount === 0){
 			$('#content').prepend(template);
