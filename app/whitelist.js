606c6091c95775b3e0941a39faa2365443de6b3f
rsmith-finix <rsmith@finixcreative.com>
2016-07-08 09:57:44 -0700

    Whitelist - github
    Add github codebase to whitelist

--- app/whitelist.js
+++ app/whitelist.js
@@ -1,9 +1,11 @@
 angular.module('finixApp')
 	.config(['$sceDelegateProvider', function($sceDelegateProvider){
 		$sceDelegateProvider.resourceUrlWhitelist([
-			// Allow same origin resource loads.
+			// Allow same origin resource loads
 			'self',
-			// Allow loading from our assets domain.  Notice the difference between * and **.
+			// Allow Finix codebase
+			'http://finixcreative.github.io/**'
+			// Allow loading from our assets domain (difference between * and **)
 			'https://www.google.com/**',
 			'https://www.youtube.com/**',
 			'https://twitter.com/**',
