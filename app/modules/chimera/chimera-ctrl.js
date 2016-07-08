bc755a8333937765bd01da6439c6f309f9c6738e
rsmith-finix <rsmith@finixcreative.com>
2016-07-08 10:47:21 -0700

    Row - tests
    Add tests for add row function

--- app/modules/chimera/chimera-ctrl.js
+++ app/modules/chimera/chimera-ctrl.js
@@ -58,6 +58,8 @@ angular.module('chimeraMod')
 		);
 		$scope.addRow = function(row, placement){
 			console.log(row, placement);
+			console.log(row.$parent);
+			console.log(row.$parent.$parent);
 			row.$parent.contents.push({
 				"config": {
 					"type": "row",
